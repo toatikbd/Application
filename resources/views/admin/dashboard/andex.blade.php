8b6bf8839a2c9028ae9309775a786a3bbdb25576
Atiqur Rahaman <toatikbd@gmail.com>
2021-04-01 14:52:52 +0600

    added all file

---
+++ resources/views/admin/dashboard/andex.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.admin')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="block-header">
+            <h2>BLANK PAGE</h2>
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/', function() {
+    return view('layouts.website');
+});
+
+
+//Admin route
+Route::get('/test', function (){
+   return view('layouts.admin');
+});
+Route::get('/index', function (){
+    return view('admin.dashboard.index');
+});
+
+//end Admin route
