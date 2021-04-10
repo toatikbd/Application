b3dce57f2642ac860cedd68d4462e2fd8c82016b
Atiqur Rahaman <toatikbd@gmail.com>
2021-04-10 12:19:36 +0600

    create site clearance controller model and migration

---
+++ app/Http/Controllers/SiteClearanceController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\SiteClearance;
+use Illuminate\Http\Request;
+
+class SiteClearanceController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SiteClearance  $siteClearance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SiteClearance $siteClearance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SiteClearance  $siteClearance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SiteClearance $siteClearance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\SiteClearance  $siteClearance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, SiteClearance $siteClearance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SiteClearance  $siteClearance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SiteClearance $siteClearance)
+    {
+        //
+    }
+}
