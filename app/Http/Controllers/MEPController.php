d82961e71fc0b0efcb68dcdb3ba2e61986b404f5
Atiqur Rahaman <toatikbd@gmail.com>
2021-04-21 11:38:21 +0600

    create mep controller model and migration file

---
+++ app/Http/Controllers/MEPController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MEP;
+use Illuminate\Http\Request;
+
+class MEPController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MEP  $mEP
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MEP $mEP)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MEP  $mEP
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MEP $mEP)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MEP  $mEP
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MEP $mEP)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MEP  $mEP
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MEP $mEP)
+    {
+        //
+    }
+}
